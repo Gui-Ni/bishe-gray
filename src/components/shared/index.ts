cccf0fa57ba6c052d0a832d42ee6103e1eef2393
Gui-Ni <gui@bishe-gray.com>
2026-04-21 13:36:49 +0800

    fix: export AchievementDetail component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,2 +1,3 @@
 export { default as SyncLogo } from './SyncLogo';
 export { default as AchievementModal } from './AchievementModal';
+export { default as AchievementDetail } from './AchievementDetail';
